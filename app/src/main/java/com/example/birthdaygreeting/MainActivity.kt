26d22f493d957bd64dff42546ef55cc5fdf1cd23
SnippyRex <kentf022@gmail.com>
2023-04-10 01:19:09 +0100

    Build: Add drawable as app background

--- app/src/main/java/com/example/birthdaygreeting/MainActivity.kt
+++ app/src/main/java/com/example/birthdaygreeting/MainActivity.kt
@@ -3,14 +3,16 @@ package com.example.birthdaygreeting
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.birthdaygreeting.ui.theme.BirthdayGreetingTheme
@@ -25,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    BirthdayGreetingWithImage(message = "Happy Birthday Snippy!", from = "- From Snippy xox" )
                 }
             }
         }
@@ -45,11 +47,22 @@ fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier =
     }
 }
 
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        BirthdayGreetingWithText(message = message, from = from )
+    }
+}
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun BirthdayCardPreview() {
     BirthdayGreetingTheme {
-        BirthdayGreetingWithText(message = "Happy Birthday Snippy!", from ="- From Snippy")
+        BirthdayGreetingWithImage(message = "Happy Birthday Snippy!", from ="- From Snippy")
     }
 }
